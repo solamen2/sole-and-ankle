a2038132f8496910bf7c21bdc8d5896615c0ff8a
Edward Younskevicius <eyounskevicius@fullsail.com>
2023-01-20 16:26:25 -0800

    Initial commit after building, and implement super header

--- src/components/SuperHeader/SuperHeader.js
+++ src/components/SuperHeader/SuperHeader.js
@@ -15,24 +15,31 @@ const SuperHeader = () => {
       </MarketingMessage>
       <SearchInput />
       <HelpLink href="/help">Help</HelpLink>
-      <UnstyledButton>
+      <UnstyledButtonSuperHeader>
         <Icon id="shopping-bag" strokeWidth={1} />
-      </UnstyledButton>
+      </UnstyledButtonSuperHeader>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
+  display: flex;
+  gap: 24px;
   font-size: 0.875rem;
   color: ${COLORS.gray[300]};
   background-color: ${COLORS.gray[900]};
+  padding: 10px 32px 6px 32px;
+  line-height: 0px;
 `;
 
 const MarketingMessage = styled.span`
+  flex: 1;
   color: ${COLORS.white};
+  padding: 0px auto 0px 0px;
 `;
 
 const HelpLink = styled.a`
+  flex: 0;
   color: inherit;
   text-decoration: none;
   outline-offset: 2px;
@@ -41,5 +48,10 @@ const HelpLink = styled.a`
     outline: none;
   }
 `;
+
+const UnstyledButtonSuperHeader = styled(UnstyledButton)`
+  flex: 0;
+  margin-top: -2px;
+`
 
 export default SuperHeader;
